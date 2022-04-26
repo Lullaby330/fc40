987399081c24759c8eb55605d2fc6c385e3cec35
Lullaby330 <96850632+Lullaby330@users.noreply.github.com>
2022-04-26 19:17:54 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -4,7 +4,7 @@ import base64
 import re
 import asyncio
 from pyrogram import filters
-from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, ADMINS
+from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
